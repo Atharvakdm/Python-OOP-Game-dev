56db522ebdc9f9df5bc4aac6220ee2169f7a6c6c
Atharva Kadam <165936212+Atharvakdm@users.noreply.github.com>
2026-01-13 23:39:02 -0500

    Modify Book class to include price and add methods

--- oop-book.py
+++ oop-book.py
@@ -1,18 +1,21 @@
 class Book:
-    def __init__(self, title, author, isbn, is_available=True):
+
+    def __init__(self, title, author, isbn, price, is_available=True):
         self.title = title
         self.author = author
         self.isbn = isbn
+        self.price = price
         self.is_available = is_available
+        self.books = []
+        self.count = 0
+        self.dict = {self.title:self.price}
+
 
     def __str__(self):
         status = "Available" if self.is_available else "Checked Out"
         return f"{self.title} by {self.author} ({status})"
 
-class Library:
-    def __init__(self, name):
-        self.name = name
-        self.books = []
+class Library(Book):
 
     def add_book(self, new_book):
         self.books.append(new_book)
@@ -30,15 +33,12 @@ class Library:
                     return
         print("Book not found in our system.")
 
+    def purchase_book(self, title):
+        self.books.remove(title)
+        print(f"{self.title} bought from the library, count:{self.count}")
+        self.count += 1
 
-# 1. Create a book
-b1 = Book("The Hobbit", "J.R.R. Tolkien", "12345")
-
-# 2. Create the library
-my_library = Library("City Central Library")
-
-# 3. Add the book object to the library's list
-my_library.add_book(b1)
-
-# Now, my_library.books contains [b1]
-print(my_library.books[0].title) # This would print: The Hobbit
+    def get_price_by_book(self, title):
+        for title, price in self.dict.items():
+            if title == self.title:
+                print(price)
